7ce397f4f2c58ff3b460b24fb692d7c9bbf53213
Ray Lam <73551712+raylamgit@users.noreply.github.com>
2023-10-26 12:48:24 -0500

    Update lgacdb01.cbl

--- base/src/lgacdb01.cbl
+++ base/src/lgacdb01.cbl
@@ -8,7 +8,7 @@
       *   To add customer's name, address and date of birth to the     *
       *  DB2 customer table creating a new customer entry.             *
       *                                                                *
-      *  Ray Lam 10/26/2023 1,2,3,4
+      *  Ray Lam 10/26/2023 1,2,3,4,5
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. LGACDB01.
